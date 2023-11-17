8658d2b15ba5e7824d5b4ddca92168f518f92518
Edson Garcia <ednosmab@yahoo.com.br>
2023-11-17 17:27:25 -0300

    creating a dictionary component

---
+++ src/Dictionary/index.js
@@ -0,0 +1,10 @@
+import Form from "./Form"
+import Search from "./Search"
+export default function Dictionary(){
+    return (
+        <>
+            <h1>Dicionário de Inglês</h1>
+            <Form/>
+        </>
+    )
+}
