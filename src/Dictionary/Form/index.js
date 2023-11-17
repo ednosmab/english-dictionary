10ded47540eeb2fa06f202d2f7413570b645b899
Edson Garcia <ednosmab@yahoo.com.br>
2023-11-17 17:27:54 -0300

    creating a form component

---
+++ src/Dictionary/Form/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Input from "../Input";
+import MyButton from "../MyButton";
+import Search from "../Search";
+
+export default function Form(props){
+
+    const [search, setSearch] = useState('')
+    const [mySearch, setMySearch] = useState('')
+
+    function toSearch(event){
+        event.preventDefault()
+        setMySearch(search)
+    }
+    
+    console.log(mySearch)
+    return (
+        <>
+            <form onSubmit={toSearch}>
+                <Input 
+                    placeholder="Digite aqui a sua pesquisa" 
+                    mandatory={true} 
+                    label="Palavra"
+                    myValue={search}
+                    toChange={value => setSearch(value)}
+                    />
+                <MyButton>
+                    Pesquisar
+                </MyButton>
+            </form>
+            {mySearch !== '' && <Search toSearch={mySearch}/>}
+        </>
+    )
+}
