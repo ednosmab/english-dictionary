bf9354740a15dfe53915c5fcea7a3b2ca5be3933
Edson Garcia <ednosmab@yahoo.com.br>
2023-11-21 20:47:45 -0300

    search by word finished

--- src/Dictionary/Form/index.js
+++ src/Dictionary/Form/index.js
@@ -13,7 +13,6 @@ export default function Form(props){
         setMySearch(search)
     }
     
-    console.log(mySearch)
     return (
         <>
             <form onSubmit={toSearch}>
--- src/Dictionary/Search/index.js
+++ src/Dictionary/Search/index.js
@@ -1,41 +1,59 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 export default function Search(props){
-    const [definitionsValue1, setDefinitions1] = useState('')
-    const [definitionsValue2, setDefinitions2] = useState('')
-    const [definitionsValue3, setDefinitions3] = useState('')
-    const [synonymsValue, setSynonyms] = useState('')
+    const [definitionsValue, setDefinitions] = useState([])
 
-    async function buscarDados(wordToSearch){
-      const resultado = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`)
-      return resultado.data
+    useEffect(() =>{
 
-    }
+      async function buscarDados(wordToSearch){
+        try {
+          const resultado = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`)
+          console.log(resultado)
+          return resultado.data
+          
+        } catch (error) {
+          return error.response.status
+        }
+  
+      }
 
-    let dados = buscarDados(props.toSearch)
-                  .then(res => res.map(res => res.meanings))
-                  .then(res2 => res2.map(res3 => res3.map(res4 => res4)))
-                       
-    let definitions = Promise.resolve(dados.then(res => res[0][0].definitions))
-    definitions.then(res => res[0] ? setDefinitions1(res[0].definition) : setDefinitions1(''))
-    definitions.then(res => res[1] ? setDefinitions2(res[1].definition) : setDefinitions2(''))
-    definitions.then(res => res[2] ? setDefinitions3(res[2].definition) : setDefinitions3(''))
-    
-    let synonyms = Promise.resolve(dados.then(res => res[0][0].synonyms[0]))
-    synonyms.then(res => setSynonyms(res))
-    
-    console.log(dados.length)
+      buscarDados(props.toSearch).then(res => setDefinitions(res))
 
-    return (
-      <>
+    }, [props.toSearch])
+
+      return (
+        <>
           <ul>
-              {definitionsValue1 && <li>{definitionsValue1}</li>}
-              {definitionsValue2 && <li>{definitionsValue2}</li>}
-              {definitionsValue3 && <li>{definitionsValue3}</li>}
-              {synonymsValue && <li>{synonymsValue}</li>}
+            {
+              !(definitionsValue === 404) ?
+              definitionsValue.map((res, index) => {
+                return index === 0 ? <h2>Word: {res.word}</h2> : ""
+              }) : "Palavra não encontrada"
+            }
+            {
+              !(definitionsValue === 404) &&
+              definitionsValue.map(res => 
+                  res.meanings.map((r, index) => {
+                    return (
+                      <>
+                        <li key={index}>
+                          {r.partOfSpeech}
+                          <ul>
+                            <li>{r.definitions.map(d => {
+                              return(
+                                <li>{d.definition}</li>
+                              )
+                            })}</li>  
+                          </ul>  
+                        </li>
+                        {console.log(r.synonyms.length)} {r.synonyms.length >= 1 && <h3>Synonimy: {r.synonyms}</h3>}
+                      </>
+                    )
+                  }))
+              
+            }
           </ul>
-      </>
-    );
-    
+        </>
+      );
 }
