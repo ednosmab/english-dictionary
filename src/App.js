2308400e95733a0c9d6b4c1c0fae644f8f33e396
Edson Garcia <ednosmab@yahoo.com.br>
2023-11-22 19:00:22 -0300

    including dictionary and lua images a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Dictionary from "./Dictionary";
+import './App.css'
+import dictionary from './dictionary.svg'
+import moon from './moon.svg'
 
 function App() {
   return (
     <div>
+      <header className="container">
+        <div className="content-container">
+          <img src={dictionary} alt=""/>
+          <img src={moon} alt=""/>
+        </div>
+      </header>
       <Dictionary/>
     </div>
   );
